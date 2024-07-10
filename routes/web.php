f7c0ac765d1751ec5e0e965f05d3989314790ec7
u226 <me@gmail.com>
2024-07-10 10:53:03 +0330

    Fix bugs

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/download', [VideoController::class, 'download']);
+Route::post('/download', [VideoController::class, 'download'])->name('download');
